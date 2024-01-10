705e418bb0bef7b8e60f00e60cd3b588c2adba37
Max Gruebmeyer <max@gruebmeyer.de>
2024-01-10 12:34:05 +0100

    Added closing of sockets on interrupt signal
    Signed-off-by: Max Gruebmeyer <max@gruebmeyer.de>

--- src/main.c
+++ src/main.c
@@ -1,5 +1,7 @@
 #include <stdio.h>
+#include <stdlib.h>
 #include <string.h>
+#include <signal.h>
 #include <errno.h>
 
 #include "errorhandler.h"
@@ -13,21 +15,25 @@
 #define MAX_REQ_SIZE 1024
 #define MAX_RES_SIZE 2 * 1024
 
+static void sighandle(int _);
 static int send_dummy_response(const int sockfd);
-static void close_socket(const int sockfd);
+static void close_socket(int *sockfd);
+
+static int sockfd = -1;
+static int connected_sockfd = -1;
 
 int main(void)
 {
-    /* AF_INET sockets can either be connection-oriented SOCK_STREAM
-     * or connectionless SOCK_DGRAM, but not SOCK_SEQPACKET!
-     */
-    const int sockfd = socket(AF_INET, SOCK_STREAM);
-    int connected_sockfd;
-
     char msg[MAX_REQ_SIZE];
     struct sockaddr addr;
 
-    if (sockfd == -1) {
+    /* Simple interrupt handler to properly shut down sockets on <Ctrl-C> */
+    signal(SIGINT, sighandle);
+
+    /* AF_INET sockets can either be connection-oriented SOCK_STREAM
+     * or connectionless SOCK_DGRAM, but not SOCK_SEQPACKET!
+     */
+    if ((sockfd = socket(AF_INET, SOCK_STREAM)) == -1) {
         return handle_socket_err();
     }
 
@@ -42,19 +48,19 @@ int main(void)
             ((unsigned char)addr.sa_data.addr.port[0] << 8) + (unsigned char)addr.sa_data.addr.port[1]);
 
     if (bind(sockfd, &addr, sizeof(addr)) != 0) {
-        close_socket(sockfd);
+        close_socket(&sockfd);
         return handle_bind_err(sockfd);
     }
 
     if (listen(sockfd, BACKLOG_SIZE) != 0) {
-        close_socket(sockfd);
+        close_socket(&sockfd);
         return handle_listen_err();
     }
 
     printf("Watiting for connections...\n");
 
     if ((connected_sockfd = accept(sockfd)) == -1) {
-        close_socket(sockfd);
+        close_socket(&sockfd);
         return handle_accept_err();
     }
 
@@ -83,19 +89,33 @@ int main(void)
     printf("Received a message: \"%s\"\n", msg);
     if (send_dummy_response(connected_sockfd) == -1) {
         printf("Sending dummy response failed!\n");
-        close_socket(connected_sockfd);
-        close_socket(sockfd);
+        close_socket(&connected_sockfd);
+        close_socket(&sockfd);
     }
 
     printf("Successfully sent dummy response!\n");
 
-    /* TODO (GM): Also close sockets on interrupt signal! */
-    close_socket(connected_sockfd);
-    close_socket(sockfd);
+    close_socket(&connected_sockfd);
+    close_socket(&sockfd);
 
     printf("Done!\n");
     return 0;
 }
+
+# pragma GCC diagnostic ignored "-Wunused-parameter"
+
+static void sighandle(int _)
+{
+    printf("\033[33mInterrupt signal received, closing sockets!\033[0m\n");
+
+    close_socket(&connected_sockfd);
+    close_socket(&sockfd);
+
+    printf("\033[33mAll sockets closed, exiting.\033[0m\n");
+    exit(0);
+}
+
+# pragma GCC diagnostic error "-Wunused-parameter"
 
 static int send_dummy_response(const int sockfd)
 {
@@ -122,10 +142,20 @@ static int send_dummy_response(const int sockfd)
     return sendto(sockfd, res, strlen(res));
 }
 
-static void close_socket(const int sockfd)
+static void close_socket(int *socket_fd)
 {
-    printf("Closing socket %i...\n", sockfd);
-    if (close(sockfd) != 0) {
-        handle_close_err(sockfd);
+    if (*socket_fd < 0) {
+        printf("Could not close socket, negative value %i implies socket already closed!\n", *socket_fd);
+        return;
     }
+
+    printf("Closing socket %i...\n", *socket_fd);
+    if (close(*socket_fd) != 0) {
+        printf("Close failed, handling error!\n");
+        handle_close_err(*socket_fd);
+    } else {
+        printf("Socket closed successfully!\n");
+    }
+
+    *socket_fd = -1;
 }
